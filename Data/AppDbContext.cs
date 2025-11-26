56c3536bfa5b8207c4aa3ed06baafcd1ad3b1026
Herbas-n <nicole.herbas@ucb.edu.bo>
2025-11-25 23:14:18 -0400

    feat: adding viajes to dbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,27 @@
-﻿namespace ProyectoFinal.Data
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Viaje> Viajes => Set<Viaje>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Viaje>(entity =>
+            {
+                entity.HasKey(v => v.Id);
+                entity.Property(v => v.Origen).IsRequired().HasMaxLength(100);
+                entity.Property(v => v.Destino).IsRequired().HasMaxLength(100);
+                entity.Property(v => v.FechaSalida).IsRequired();
+                entity.Property(v => v.Precio).IsRequired().HasColumnType("decimal(18,2)");
+                entity.Property(v => v.Estado).IsRequired().HasMaxLength(50);
+            });
+        }
     }
 }
