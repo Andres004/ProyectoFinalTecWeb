e602cd8841e3ad245d75d2a143e8362e1a001d2b
Herbas-n <nicole.herbas@ucb.edu.bo>
2025-11-26 01:08:09 -0400

    feat: creating login dtos

--- Models/Conductor.cs
+++ Models/Conductor.cs
@@ -4,10 +4,22 @@
     {
         public Guid Id { get; set; }
         public string Nombre { get; set; }
-        public string LicenciaConducir { get; set; }
-        public string Telefono { get; set; }
+        public string LicenciaConducir { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } = "Conductor";//"Pasajero" | "Conductor"
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiresAt { get; set; }
+        public DateTime? RefreshTokenRevokedAt { get; set; }
+        public string? CurrentJwtId { get; set; }
+
+
 
         // 1:N Conductor -> Viajes
         public ICollection<Viaje> Viajes { get; set; } = new List<Viaje>();
+
+        // N:M Conductor <-> Vehiculos
     }
 }
